51595d41484c573f9bf72531d1dbee50fbfd6fda
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-04-03 19:15:09 -0600

    Agregando lista de compra y formulario de pago

---
+++ PruebaPractica2DSM_LA181955_MA181956/app/src/main/java/org/example/pruebapractica2dsm_la181955_ma181956/PagoActivity.kt
@@ -0,0 +1,11 @@
+package org.example.pruebapractica2dsm_la181955_ma181956
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pago)
+    }
+}
